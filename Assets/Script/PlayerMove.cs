60fc4a2b361500f4e2e369ff5da8970b57862246
0moknuni <114228828+0moknuni@users.noreply.github.com>
2024-11-29 12:39:47 +0900

    보간 수정
    1. 보간 작동 타이밍 동기화
    2. 보간 작동 중 여부를 검사하여 조정해주는 래퍼함수 작성

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -2,8 +2,10 @@ using Newtonsoft.Json;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.UI;
 
 public class PlayerMove : MonoBehaviour
 { 
@@ -15,6 +17,9 @@ public class PlayerMove : MonoBehaviour
     Rigidbody m_Rigidbody;
     Vector3 movement;
     //Vector3 nextPosition;
+
+    Coroutine selfUpdate = null;
+    Coroutine otherUpdate = null;
 
     //IPacket packet;
     //TODO - 서버와 연결 테스트 후 캡슐화하기
@@ -50,40 +55,85 @@ public class PlayerMove : MonoBehaviour
     //    return playerPacket;
     //}
 
+    public void MoveSelf()
+    {
+        if (selfUpdate != null)
+            StopCoroutine(selfUpdate);
 
-    public PlayerPacket OtherPlayerUpdate(PlayerPacket _pp)
+        selfUpdate = StartCoroutine(UpdateSelf());
+    }
+
+    public void MoveOther(PlayerPacket _pp)
+    {
+        if (otherUpdate != null)
+            StopCoroutine(otherUpdate);
+
+        otherUpdate = StartCoroutine(UpdateOther(_pp));
+    }
+   
+    private IEnumerator UpdateOther(PlayerPacket _pp)
     {
         if (_pp != null)
         {
-            transform.position += _pp.GetPosition2Vec3();
-        }
+            float o_currentTime = 0;    //other
+            float o_moveTime = 0.08f;
+            Vector3 o_tmpPos;
 
-        _pp.SetPosition(movement);
-        
-        return _pp; 
+            if (o_currentTime != 0)
+                o_currentTime = 0;
+
+            while (o_currentTime < o_moveTime)  //부드러운 이동 가능
+            {
+                if (o_currentTime >= o_moveTime)
+                {
+                    o_currentTime = o_moveTime;
+                }
+
+                yield return null;
+
+                o_currentTime += Time.deltaTime;
+
+                o_tmpPos = transform.position + _pp.GetPosition2Vec3();
+                o_tmpPos.y = transform.position.y;
+                transform.position = Vector3.Lerp(transform.position, o_tmpPos, o_currentTime / o_moveTime);
+            }
+        }
     }
 
-    public void DebugMoveSelf()
+
+    private IEnumerator UpdateSelf()
     {
-        //if (playerPacket == null)
-        //    playerPacket = new PlayerPacket();
-
-        //if (playerPacket != null)
-        //{
-        //    if (playerPacket.ClientNum == GameManager.Instance.GetSelfClientNum())
-        //        transform.position += playerPacket.GetPosition2Vec3();
-        //}
-
         if (playerPacket == null)
-            return;
+            yield break;
+
+        float s_currentTime = 0;    //self
+        float s_moveTime = 0.08f;
+        Vector3 s_tmpPos;
 
         //TODO - 로컬 변수 추가
         if (GameManager.Instance.isStarting)
         {
             if (Move())
             {
-                Debug.Log("Move : " + GameManager.Instance.networkManager.sendQue.Count);
-                transform.position += movement;
+                if (s_currentTime != 0)
+                    s_currentTime = 0;
+
+                while (s_currentTime < s_moveTime)  //부드러운 이동 가능
+                {
+                    if (s_currentTime >= s_moveTime)
+                    {
+                        s_currentTime = s_moveTime;
+                    }
+
+                    yield return null;
+
+                    s_currentTime += Time.deltaTime;
+
+                    s_tmpPos = transform.position + movement;
+                    s_tmpPos.y = transform.position.y;
+                    transform.position = Vector3.Lerp(transform.position, s_tmpPos, s_currentTime / s_moveTime);
+                }
+
                 playerPacket.SetPosition(movement);
                 playerPacket.clientNum = GameManager.Instance.GetSelfClientNum();
                 GameManager.Instance.networkManager.sendQue.Enqueue(playerPacket);
